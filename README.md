7291c81e907914df5e825cae5c713091ca30b311
Jose Flores <jop0693@gmail.com>
2023-08-30 07:05:59 -0600

    documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# Websocket Server Template
+Basic template that allows a single websocket route with a multiple handler of websocket messages
+
+## Startup
+```sh
+$ yarn install
+$ yarn docs
+$ yarn start
+```
+
+## Documentation generator
+To add new documentation follow the next steps:  
+1. Add a new schema on `src/schemas` and export the zodToJsonSchema variable
+2. Add the new schema on `src/lib/documentation` to be written to a json file
+3. On `documentation/asyncapi.yaml` create a new `channel`
+4. Run `yarn docs` to produce the html output
--- src/lib/documentation.ts
+++ src/lib/documentation.ts
@@ -3,14 +3,24 @@ import fs from 'node:fs/promises';
 import { authenticationJsonSchema } from '../schemas/authentication';
 
 const rootProjectPath = path.resolve(__dirname, '../..');
+const documentationSchemaPath = rootProjectPath + '/documentation/schemas';
 
-async function generateDocumentationSchemas() {
-  const documentationSchemaPath = rootProjectPath + '/documentation/schemas';
-  try {
-    await fs.writeFile(
+function schemaGenerator(): Promise<void>[] {
+  // Add here the new schema that needs to be generated
+  const promises = [
+    fs.writeFile(
       documentationSchemaPath + '/authentication.json',
       JSON.stringify(authenticationJsonSchema)
-    );
+    ),
+  ];
+  return promises;
+}
+
+async function generateDocumentationSchemas() {
+  console.log('generating new schemas documentation');
+  try {
+    const schemas = schemaGenerator();
+    await Promise.all(schemas);
   } catch (err) {
     console.error(err);
   }
